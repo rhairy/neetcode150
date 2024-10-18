5747f1a5794c1ba1bce36e183f66a76ec1cba5f0
Randall Harrison <rharrison@icwgroup.com>
2024-10-18 10:30:12 -0700

    add isAnagram solution and tests

--- neet150.py
+++ neet150.py
@@ -7,3 +7,9 @@ class Solution:
                 return True
             i = i + 1
         return False
+    
+    def isAnagram(self, s: str, t: str) -> bool:
+        if sorted(s) == sorted(t):
+            return True
+        else:
+            return False
--- tests/test_neet150.py
+++ tests/test_neet150.py
@@ -9,3 +9,7 @@ def solution_object():
 def test_hasDuplicate(solution_object):
     assert solution_object.hasDuplicate([1,2,3,4]) == False
     assert solution_object.hasDuplicate([1,2,3,3]) == True
+
+def test_isAnagram(solution_object):
+    assert solution_object.isAnagram("racecar", "carrace") == True
+    assert solution_object.isAnagram("jar", "jam") == False
